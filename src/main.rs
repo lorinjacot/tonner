01ecc50cbf910231073ea1951a6ac132d0338ccb
lorinjacot <71598369+lorinjacot@users.noreply.github.com>
2025-01-31 16:12:34 -0800

    tracing added and configured

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use lightning::run;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 fn main() {
-    env_logger::init();
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
 
     run();
 }

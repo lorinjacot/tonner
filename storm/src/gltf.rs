c7e8e45102749d1d639b42661f7cc53ea28df03f
Lorin Jacot <71598369+lorinjacot@users.noreply.github.com>
2025-06-15 19:13:24 -0700

    Animation

--- storm/src/gltf.rs
+++ storm/src/gltf.rs
@@ -166,6 +166,110 @@ struct SparseAccessorValues {
     byte_offset: usize,
 }
 
+/// A keyframe animation.
+#[derive(Serialize, Deserialize)]
+struct Animation {
+    /// An array of animation channels. An animation channel combines an
+    /// animation sampler with a target property being animated. Different
+    /// channels of the same animation **MUST NOT** have the same targets.
+    channels: Vec<AnimationChannel>,
+
+    /// An array of animation samplers. An animation sampler combines timestamps
+    /// with a sequence of output values and defines an interpolation algorithm.
+    samplers: Vec<AnimationSampler>,
+
+    /// The user-defined name of this object. This is not necessarily unique, e.g.,
+    /// an accessor and a buffer could have the same name, or two accessors could
+    /// even have the same name.
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
+}
+
+/// An animation channel combines an animation sampler with a target property being animated.
+#[derive(Serialize, Deserialize)]
+struct AnimationChannel {
+    /// The index of a sampler in this animation used to compute the value for the target,
+    /// e.g., a node’s translation, rotation, or scale (TRS).
+    sampler: usize,
+
+    /// The descriptor of the animated property.
+    target: AnimationTarget,
+}
+
+/// The descriptor of the animated property.
+#[derive(Serialize, Deserialize)]
+struct AnimationTarget {
+    /// The index of the node to animate. When undefined, the animated object
+    /// **MAY** be defined by an extension.
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    node: Option<usize>,
+
+    /// The name of the node’s TRS property to animate, or the "weights" of
+    /// the Morph Targets it instantiates. For the [Translation](AnimationTargetPath::Translation)
+    /// property, the values that are provided by the sampler are the translation along
+    /// the X, Y, and Z axes. For the [Rotation](AnimationTargetPath::Rotation) property,
+    /// the values are a quaternion in the order (x, y, z, w), where w is the scalar.
+    /// For the [Scale](AnimationTargetPath::Scale) property, the values are the scaling
+    /// factors along the X, Y, and Z axes.
+    path: AnimationTargetPath,
+}
+
+/// The name of the node’s TRS property to animate, or the "weights" of
+/// the Morph Targets it instantiates. For the [Translation](AnimationTargetPath::Translation)
+/// property, the values that are provided by the sampler are the translation along
+/// the X, Y, and Z axes. For the [Rotation](AnimationTargetPath::Rotation) property,
+/// the values are a quaternion in the order (x, y, z, w), where w is the scalar.
+/// For the [Scale](AnimationTargetPath::Scale) property, the values are the scaling
+/// factors along the X, Y, and Z axes.
+#[derive(Serialize, Deserialize)]
+enum AnimationTargetPath {
+    Translation,
+    Rotation,
+    Scale,
+    Weights,
+}
+
+/// An animation sampler combines timestamps with a sequence of output values and defines an interpolation algorithm.
+#[derive(Serialize, Deserialize)]
+struct AnimationSampler {
+    /// The index of an accessor containing keyframe timestamps. The accessor **MUST** be of scalar type with
+    /// floating-point components. The values represent time in seconds with `time[0] >= 0.0`, and strictly
+    /// increasing values, i.e., `time[n + 1] > time[n]`.
+    input: usize,
+
+    /// Interpolation algorithm.
+    #[serde(default)]
+    interpolation: AnimationInterpolation,
+
+    /// The index of an accessor, containing keyframe output values.
+    output: usize,
+}
+
+/// Interpolation algorithm.
+#[derive(Default, Serialize, Deserialize)]
+enum AnimationInterpolation {
+    /// The animated values are linearly interpolated between keyframes.
+    /// When targeting a rotation, spherical linear interpolation (slerp)
+    /// **SHOULD** be used to interpolate quaternions. The number of
+    /// output elements **MUST** equal the number of input elements.
+    #[default]
+    LINEAR,
+
+    /// The animated values remain constant to the output of the first keyframe,
+    /// until the next keyframe. The number of output elements **MUST** equal the
+    /// number of input elements.
+    STEP,
+
+    /// The animation’s interpolation is computed using a cubic spline with
+    /// specified tangents. The number of output elements **MUST** equal three
+    /// times the number of input elements. For each input element, the output
+    /// stores three elements, an in-tangent, a spline vertex, and an out-tangent.
+    /// There **MUST** be at least two keyframes when using this interpolation.
+    CUBICSPLINE,
+}
+
 fn is_0(value: &usize) -> bool {
     *value == 0
 }

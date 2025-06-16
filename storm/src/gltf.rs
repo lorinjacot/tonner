b1ea9c03f4ff970e333aaeb226fadc3b512e1d6f
Lorin Jacot <71598369+lorinjacot@users.noreply.github.com>
2025-06-15 20:40:14 -0700

    material

--- storm/src/gltf.rs
+++ storm/src/gltf.rs
@@ -583,8 +583,236 @@ impl ImageMimeType {
     }
 }
 
+/// The material appearance of a primitive.
 #[derive(Serialize, Deserialize)]
-struct Material {}
+struct Material {
+    /// The user-defined name of this object. This is not necessarily unique, e.g.,
+    /// an accessor and a buffer could have the same name, or two accessors could
+    /// even have the same name.
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
+
+    /// A set of parameter values that are used to define the metallic-roughness material
+    /// model from Physically Based Rendering (PBR) methodology. When undefined, all the
+    /// default values of [PbrMetallicRoughness] **MUST** apply.
+    #[serde(rename = "pbrMetallicRoughness")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "PbrMetallicRoughness::is_default")]
+    pbr_metallic_roughness: PbrMetallicRoughness,
+
+    /// The tangent space normal texture. The texture encodes RGB components with linear
+    /// transfer function. Each texel represents the XYZ components of a normal vector in
+    /// tangent space. The normal vectors use the convention +X is right and +Y is up. +Z
+    /// points toward the viewer. If a fourth component (A) is present, it **MUST** be ignored.
+    /// When undefined, the material does not have a tangent space normal texture.
+    #[serde(rename = "normalTexture")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    normal_texture: Option<MaterialNormalTexture>,
+
+    /// The occlusion texture. The occlusion values are linearly sampled from the R channel.
+    /// Higher values indicate areas that receive full indirect lighting and lower values
+    /// indicate no indirect lighting. If other channels are present (GBA), they **MUST** be ignored
+    /// for occlusion calculations. When undefined, the material does not have an occlusion texture.
+    #[serde(rename = "occlusionTexture")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    occlusion_texture: Option<MaterialOcclusionTexture>,
+
+    /// The emissive texture. It controls the color and intensity of the light being emitted by the material. $
+    /// This texture contains RGB components encoded with the sRGB transfer function. If a fourth component (A) is present,
+    /// it **MUST** be ignored. When undefined, the texture **MUST** be sampled as having 1.0 in RGB components.
+    #[serde(rename = "emissiveTexture")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    emissive_texture: Option<MaterialTexture>,
+
+    /// The factors for the emissive color of the material. This value defines
+    /// linear multipliers for the sampled texels of the emissive texture.
+    #[serde(rename = "emissiveFactor")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "is_3x00")]
+    emissive_factor: [f32; 3],
+
+    /// The material’s alpha rendering mode enumeration specifying the interpretation of the alpha value of the base color.
+    #[serde(rename = "alphaMode")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "AlphaMode::is_default")]
+    alpha_mode: AlphaMode,
+
+    /// Specifies the cutoff threshold when in [AlphaMode::MASK]. If the alpha value is
+    /// greater than or equal to this value then it is rendered as fully opaque, otherwise,
+    /// it is rendered as fully transparent. A value greater than 1.0 will render the entire
+    /// material as fully transparent. This value **MUST** be ignored for other alpha modes.
+    /// When [Material::alpha_mode] is not defined, this value **MUST NOT** be defined.
+    #[serde(rename = "alphaCutoff")]
+    #[serde(skip_serializing_if = "is_05")]
+    alpha_cutoff: f32,
+
+    /// Specifies whether the material is double sided. When this value is false,
+    /// back-face culling is enabled. When this value is true, back-face culling is
+    /// disabled and double-sided lighting is enabled. The back-face **MUST** have
+    /// its normals reversed before the lighting equation is evaluated.
+    #[serde(rename = "doubleSided")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "is_false")]
+    double_sided: bool,
+}
+
+/// A set of parameter values that are used to define the metallic-roughness material model
+/// from Physically-Based Rendering (PBR) methodology.
+#[derive(Serialize, Deserialize)]
+struct PbrMetallicRoughness {
+    /// The factors for the base color of the material. This value defines linear multipliers
+    /// for the sampled texels of the base color texture.
+    #[serde(rename = "baseColorFactor")]
+    #[serde(skip_serializing_if = "is_4x10")]
+    base_color_factor: [f32; 4],
+
+    /// The base color texture. The first three components (RGB) **MUST** be encoded with the
+    /// sRGB transfer function. They specify the base color of the material. If the fourth
+    /// component (A) is present, it represents the linear alpha coverage of the material.
+    /// Otherwise, the alpha coverage is equal to 1.0. The [Material::alpha_mode] property
+    /// specifies how alpha is interpreted. The stored texels **MUST NOT** be premultiplied.
+    /// When undefined, the texture **MUST** be sampled as having `1.0` in all components.
+    #[serde(rename = "baseColorTexture")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    base_color_texture: Option<MaterialTexture>,
+
+    /// The factor for the metalness of the material. This value defines a linear multiplier
+    /// or the sampled metalness values of the metallic-roughness texture.
+    #[serde(rename = "metallicFactor")]
+    #[serde(skip_serializing_if = "is_10")]
+    metallic_factor: f32,
+
+    /// The factor for the roughness of the material. This value defines a linear multiplier
+    /// for the sampled roughness values of the metallic-roughness texture.
+    #[serde(rename = "roughnessFactor")]
+    #[serde(skip_serializing_if = "is_10")]
+    roughness_factor: f32,
+
+    /// The metallic-roughness texture. The metalness values are sampled from the B channel.
+    /// The roughness values are sampled from the G channel. These values **MUST** be encoded
+    /// with a linear transfer function. If other channels are present (R or A), they **MUST**
+    /// be ignored for metallic-roughness calculations. When undefined, the texture **MUST**
+    /// be sampled as having 1.0 in G and B components.
+    #[serde(rename = "metallicRoughnessTexture")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    metallic_roughness_texture: Option<MaterialTexture>,
+}
+
+impl PbrMetallicRoughness {
+    fn is_default(&self) -> bool {
+        self.base_color_factor == [1.0; 4]
+            && self.base_color_texture.is_none()
+            && self.metallic_factor == 1.0
+            && self.roughness_factor == 1.0
+            && self.metallic_roughness_texture.is_none()
+    }
+}
+
+impl Default for PbrMetallicRoughness {
+    fn default() -> Self {
+        PbrMetallicRoughness {
+            base_color_factor: [1.0; 4],
+            base_color_texture: None,
+            metallic_factor: 1.0,
+            roughness_factor: 1.0,
+            metallic_roughness_texture: None,
+        }
+    }
+}
+
+/// Reference to a texture.
+#[derive(Serialize, Deserialize)]
+struct MaterialTexture {
+    /// The index of the texture.
+    index: usize,
+
+    /// This integer value is used to construct a string in the format `TEXCOORD_<set index>`
+    /// which is a reference to a key in [MeshPrimitive.attributes] (e.g. a value of `0` corresponds
+    /// to `TEXCOORD_0`). A mesh primitive **MUST** have the corresponding texture coordinate attributes
+    /// for the material to be applicable to it.
+    #[serde(rename = "texCoord")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "is_0")]
+    tex_coord: usize,
+}
+
+/// Reference to a texture.
+#[derive(Serialize, Deserialize)]
+struct MaterialNormalTexture {
+    /// The index of the texture.
+    index: usize,
+
+    /// This integer value is used to construct a string in the format `TEXCOORD_<set index>`
+    /// which is a reference to a key in [MeshPrimitive.attributes] (e.g. a value of `0` corresponds
+    /// to `TEXCOORD_0`). A mesh primitive **MUST** have the corresponding texture coordinate attributes
+    /// for the material to be applicable to it.
+    #[serde(rename = "texCoord")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "is_0")]
+    tex_coord: usize,
+
+    /// The scalar parameter applied to each normal vector of the texture. This value scales
+    /// the normal vector in X and Y directions using the formula:
+    /// `scaledNormal = normalize(<sampled normal texture value> * 2.0 - 1.0) * vec3(<normal scale>, <normal scale>, 1.0)`.
+    #[serde(skip_serializing_if = "is_10")]
+    scale: f32,
+}
+
+/// Reference to a texture.
+#[derive(Serialize, Deserialize)]
+struct MaterialOcclusionTexture {
+    /// The index of the texture.
+    index: usize,
+
+    /// This integer value is used to construct a string in the format `TEXCOORD_<set index>`
+    /// which is a reference to a key in [MeshPrimitive.attributes] (e.g. a value of `0` corresponds
+    /// to `TEXCOORD_0`). A mesh primitive **MUST** have the corresponding texture coordinate attributes
+    /// for the material to be applicable to it.
+    #[serde(rename = "texCoord")]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "is_0")]
+    tex_coord: usize,
+
+    /// A scalar parameter controlling the amount of occlusion applied. A value of `0.0` means no occlusion.
+    /// A value of `1.0` means full occlusion. This value affects the final occlusion value as:
+    /// `1.0 + strength * (<sampled occlusion texture value> - 1.0)`.
+    #[serde(skip_serializing_if = "is_10")]
+    strength: f32,
+}
+
+/// The material’s alpha rendering mode enumeration specifying the interpretation of the alpha value of the base color.
+#[derive(Default, Serialize, Deserialize)]
+enum AlphaMode {
+    /// The alpha value is ignored, and the rendered output is fully opaque.
+    #[default]
+    OPAQUE,
+
+    /// The rendered output is either fully opaque or fully transparent depending
+    /// on the alpha value and the specified [Material::alpha_cutoff] value;
+    /// the exact appearance of the edges **MAY** be subject to implementation-specific
+    /// techniques such as “Alpha-to-Coverage”.
+    MASK,
+
+    /// The alpha value is used to composite the source and destination areas.
+    /// The rendered output is combined with the background using the normal painting operation
+    /// (i.e. the Porter and Duff over operator).
+    BLEND,
+}
+
+impl AlphaMode {
+    fn is_default(&self) -> bool {
+        match self {
+            AlphaMode::OPAQUE => true,
+            _ => false,
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 struct Mesh {}
@@ -610,4 +838,20 @@ fn is_0(value: &usize) -> bool {
 
 fn is_false(value: &bool) -> bool {
     *value == false
+}
+
+fn is_3x00(value: &[f32; 3]) -> bool {
+    *value == [0.0; 3]
+}
+
+fn is_05(value: &f32) -> bool {
+    *value == 0.5
+}
+
+fn is_10(value: &f32) -> bool {
+    *value == 1.0
+}
+
+fn is_4x10(value: &[f32; 4]) -> bool {
+    *value == [1.0; 4]
 }

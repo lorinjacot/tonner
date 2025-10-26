f78360e8af81bde8188f9b600b188bbddc3892cf
Lorin Jacot <71598369+lorinjacot@users.noreply.github.com>
2025-10-26 15:05:35 +0100

    glam-js Vec3 partially written

---
+++ glam-js/src/lib.rs
@@ -0,0 +1,44 @@
+mod f32;
+
+#[macro_export]
+macro_rules! wrapper {
+    (
+        $doc:literal,
+        $wrapper:ident,
+        $inner:ty, 
+        fields: [$($field:ident: $field_type:ty),*],
+        consts: [$(
+            $const_doc:literal
+            $const:ident
+        ),*]
+    ) => {
+        #[doc = $doc]
+        #[wasm_bindgen]
+        pub struct $wrapper($inner);
+
+        paste::paste! {
+            #[wasm_bindgen]
+            impl $wrapper {
+                $(
+                    #[wasm_bindgen(getter)]
+                    pub fn $field(&self) -> $field_type {
+                        self.0.$field
+                    }
+
+                    #[wasm_bindgen(setter)]
+                    pub fn [<set_ $field>](&mut self, $field: $field_type) {
+                        self.0.$field = $field
+                    }
+                )*
+
+                $(
+                    #[doc = $const_doc]
+                    #[allow(non_snake_case)]
+                    pub fn $const() -> Self {
+                        Self($inner::$const)
+                    }
+                )*
+            }
+        }
+    };
+}

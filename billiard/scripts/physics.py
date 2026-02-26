7cdadb765ad191dd3ab220c543257022b96f54cb
lorinjacot <lorinjacot@protonmail.ch>
2026-02-26 09:30:22 +0100

    refactor (billiard): xpbd constraint, now working

--- billiard/scripts/physics.py
+++ billiard/scripts/physics.py
@@ -1,8 +1,49 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
-from numpy.typing import NDArray
 
 g = np.array([0.0, 0.0, 0.0])
 n = 100
+
+        
+class Constraint(ABC):
+    @abstractmethod
+    def __call__(self, pos: np.ndarray) -> np.floating:
+        pass
+
+    @abstractmethod
+    def grad(self, pos: np.ndarray) -> np.ndarray:
+        pass
+
+
+class DistanceConstraint(Constraint):
+    L0 = 0.05
+
+    def __init__(self, obj1: int, obj2: int) -> None:
+        self.obj1 = obj1
+        self.obj2 = obj2
+
+    def __call__(self, pos: np.ndarray) -> np.floating:
+        return max(
+            np.linalg.norm(pos[self.obj2] - pos[self.obj1]) - self.L0,
+            np.float64(0.0)
+        )
+    
+    def grad(self, pos: np.ndarray) -> np.ndarray:
+        delta: np.ndarray = pos[self.obj1] - pos[self.obj2]
+        l = np.linalg.norm(delta)
+
+        grad = np.zeros(pos.shape)
+
+        if l > 0:
+            grad[self.obj1] = delta / l
+            grad[self.obj2] = - delta / l
+
+        return grad
+
+    def delta_pos(self, pos: np.ndarray, obj: int) -> np.ndarray:
+        return self.grad(pos)[obj]
+
 
 def simulate(delta_time: float, balls: list, reset: bool):
     pos = np.stack([ball.node.local_translation for ball in balls])
@@ -28,37 +69,16 @@ def simulate(delta_time: float, balls: list, reset: bool):
         pos = pos + dt * vel
 
         constraints = [
-            DistanceConstraint(0, 1, pos),
-            DistanceConstraint(0, 2, pos),
-            DistanceConstraint(1, 2, pos),
+            DistanceConstraint(0, 1),
+            DistanceConstraint(0, 2),
+            DistanceConstraint(1, 2),
         ]
         for c in constraints:
             for i in range(len(balls)):
-                pos[i] += c.delta_pos(i)
+                pos[i] += c.delta_pos(pos, i)
 
         vel = (pos - old_pos) / dt
 
     for i in range(len(balls)):
         balls[i].node.local_translation = pos[i]
         balls[i].velocity = vel[i]
-
-class DistanceConstraint:
-    MIN_DISTANCE = 0.05
-
-    def __init__(self, obj1: int, obj2: int, pos: NDArray[np.float64]) -> None:
-        self.obj1 = obj1
-        self.obj2 = obj2
-        delta = pos[obj2] - pos[obj1]
-        l = np.linalg.norm(delta)
-        if l < self.MIN_DISTANCE:
-            self.factor = (l - self.MIN_DISTANCE) * (delta / l)
-        else:
-            self.factor = np.zeros(3)
-
-    def delta_pos(self, obj: int) -> NDArray[np.float64]:
-        if obj == self.obj1:
-            return 0.01 * 0.5 * self.factor
-        elif obj == self.obj2:
-            return - 0.01 * 0.5 * self.factor
-        else:
-            return np.zeros(3)

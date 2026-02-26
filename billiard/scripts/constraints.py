afcd56a4b114ff656007dbee174d9c2f339b46e9
Lorin Jacot <71598369+lorinjacot@users.noreply.github.com>
2026-02-26 17:16:17 +0100

    feat (billiard): border constraint

--- billiard/scripts/constraints.py
+++ billiard/scripts/constraints.py
@@ -12,7 +12,7 @@ class Constraint(ABC):
         pass
 
     def alpha(self) -> np.floating:
-        return np.float64(0.0001)
+        return np.float64(0.00001)
 
 
 class DistanceConstraint(Constraint):
@@ -43,14 +43,38 @@ class DistanceConstraint(Constraint):
 
 
 class TableConstraint(Constraint):
+    HALF_LONG_SIDE = 1.23
+    HALF_SHORT_SIDE = 0.63
     BALL_RADIUS = 0.025
 
+    MAX_X = HALF_SHORT_SIDE - BALL_RADIUS
+    MIN_X = - MAX_X
+
+    MIN_Y = BALL_RADIUS
+
+    MAX_Z = HALF_LONG_SIDE - BALL_RADIUS
+    MIN_Z = - MAX_Z
+
     def __call__(self, pos: np.ndarray) -> np.floating:
-        return np.sum(np.minimum(pos[:,1] - self.BALL_RADIUS, 0))
+        return (
+            np.sum(np.minimum(self.MAX_X - pos[:,0], 0))
+            + np.sum(np.minimum(pos[:,0] - self.MIN_X, 0))
+            + np.sum(np.minimum(pos[:,1] - self.MIN_Y, 0))
+            + np.sum(np.minimum(self.MAX_Z - pos[:,2], 0))
+            + np.sum(np.minimum(pos[:,2] - self.MIN_Z, 0))
+        )
 
     def grad(self, pos: np.ndarray) -> np.ndarray:
+        xs = pos[:,0]
+        ys = pos[:,1]
+        zs = pos[:,2]
+
         grad = np.zeros(pos.shape)
-        grad[:,1] = pos[:,1] < self.BALL_RADIUS
+        grad[:,0][xs > self.MAX_X] = - 1.0
+        grad[:,0][xs < self.MIN_X] = 1.0
+        grad[:,1] = ys < self.MIN_Y
+        grad[:,2][zs > self.MAX_Z] = - 1.0
+        grad[:,2][zs < self.MIN_Z] = 1.0
         return grad
     
     def alpha(self) -> np.floating:

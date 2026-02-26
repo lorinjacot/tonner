3afec397e6134b75222642c424381987fd614c4c
Lorin Jacot <71598369+lorinjacot@users.noreply.github.com>
2026-02-26 14:59:07 +0100

    fix (billiard): constrain def and solving fixed

--- billiard/scripts/physics.py
+++ billiard/scripts/physics.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 g = np.array([0.0, 0.0, 0.0])
-n = 100
+N = 100
 
         
 class Constraint(ABC):
@@ -15,6 +15,9 @@ class Constraint(ABC):
     def grad(self, pos: np.ndarray) -> np.ndarray:
         pass
 
+    def alpha(self) -> np.floating:
+        return np.float64(0.0001)
+
 
 class DistanceConstraint(Constraint):
     L0 = 0.05
@@ -24,25 +27,30 @@ class DistanceConstraint(Constraint):
         self.obj2 = obj2
 
     def __call__(self, pos: np.ndarray) -> np.floating:
-        return max(
-            np.linalg.norm(pos[self.obj2] - pos[self.obj1]) - self.L0,
-            np.float64(0.0)
-        )
-    
+        l =  np.linalg.norm(pos[self.obj2] - pos[self.obj1])
+        if l < self.L0:
+            return l - self.L0
+        else:
+            return np.float64(0.0)
+
     def grad(self, pos: np.ndarray) -> np.ndarray:
         delta: np.ndarray = pos[self.obj1] - pos[self.obj2]
         l = np.linalg.norm(delta)
 
         grad = np.zeros(pos.shape)
 
-        if l > 0:
+        if l < self.L0:
             grad[self.obj1] = delta / l
             grad[self.obj2] = - delta / l
 
         return grad
 
-    def delta_pos(self, pos: np.ndarray, obj: int) -> np.ndarray:
-        return self.grad(pos)[obj]
+
+constraints = [
+    DistanceConstraint(0, 1),
+    DistanceConstraint(0, 2),
+    DistanceConstraint(1, 2),
+]
 
 
 def simulate(delta_time: float, balls: list, reset: bool):
@@ -51,31 +59,29 @@ def simulate(delta_time: float, balls: list, reset: bool):
 
     if reset:
         pos = np.array([
-            [0.3, 0.0, 0.0],
-            [0.15, 0.0, 0.0],
+            [0.5, 0.0, 0.0],
             [0.0, 0.0, 0.0],
+            [-0.06, 0.0, 0.0],
         ])
         vel = np.array([
+            [-0.5, 0.0, 0.0],
             [0.0, 0.0, 0.0],
             [0.0, 0.0, 0.0],
-            [0.3, 0.0, 0.0],
         ])
 
-    dt = delta_time / n
+    dt = delta_time / N
 
-    for _ in range(n):
+    for _ in range(N):
         vel = vel + dt * g
         old_pos = pos
         pos = pos + dt * vel
 
-        constraints = [
-            DistanceConstraint(0, 1),
-            DistanceConstraint(0, 2),
-            DistanceConstraint(1, 2),
-        ]
         for c in constraints:
-            for i in range(len(balls)):
-                pos[i] += c.delta_pos(pos, i)
+            loss = c(pos)
+            if np.abs(loss) > 1e-6:
+                grad = c.grad(pos)
+                lambd = - loss / (np.sum(np.square(grad)) + c.alpha() / dt**2)
+                pos = pos + lambd * grad
 
         vel = (pos - old_pos) / dt
 

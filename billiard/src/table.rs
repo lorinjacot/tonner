d5c63503ea95d63c43a2e0ea481084dfc304f477
Lorin Jacot <71598369+lorinjacot@users.noreply.github.com>
2026-03-16 17:20:19 +0100

    billiard: table with holes

--- billiard/src/table.rs
+++ billiard/src/table.rs
@@ -10,18 +10,28 @@ pub fn table(scene_graph: &mut SceneGraph, ctx: &Context) -> MeshInstance {
     #[rustfmt::skip]
     let surface = BoxBuilder::default().name("Table surface").width(1.3).height(0.1).depth(2.5).translate(Vec3::NEG_Y * 0.05).build(ctx);
 
-    let long_borders = BoxBuilder::default().width(0.02).height(0.1).depth(2.5);
-    let long_border1 = long_borders
+    let long_borders = BoxBuilder::default().width(0.02).height(0.1).depth(1.1);
+    let long_border1a = long_borders
         .clone()
-        .name("Table long border 1")
-        .translate(vec3(-0.64, 0.05, 0.0))
+        .name("Table long border 1a")
+        .translate(vec3(-0.64, 0.05, 0.6))
         .build(ctx);
-    let long_border2 = long_borders
-        .name("Table long border 2")
-        .translate(vec3(0.64, 0.05, 0.0))
+    let long_border1b = long_borders
+        .clone()
+        .name("Table long border 1b")
+        .translate(vec3(-0.64, 0.05, -0.6))
+        .build(ctx);
+    let long_border2a = long_borders
+        .clone()
+        .name("Table long border 2a")
+        .translate(vec3(0.64, 0.05, 0.6))
+        .build(ctx);
+    let long_border2b = long_borders
+        .name("Table long border 2b")
+        .translate(vec3(0.64, 0.05, -0.6))
         .build(ctx);
 
-    let short_borders = BoxBuilder::default().width(1.26).height(0.1).depth(0.02);
+    let short_borders = BoxBuilder::default().width(1.1).height(0.1).depth(0.02);
     let short_border1 = short_borders
         .clone()
         .name("Table short border 1")
@@ -46,8 +56,10 @@ pub fn table(scene_graph: &mut SceneGraph, ctx: &Context) -> MeshInstance {
 
     MeshBuilder::default()
         .name("Table")
-        .primitive(long_border1, table_material.clone())
-        .primitive(long_border2, table_material.clone())
+        .primitive(long_border1a, table_material.clone())
+        .primitive(long_border1b, table_material.clone())
+        .primitive(long_border2a, table_material.clone())
+        .primitive(long_border2b, table_material.clone())
         .primitive(short_border1, table_material.clone())
         .primitive(short_border2, table_material.clone())
         .primitive(surface, table_material)

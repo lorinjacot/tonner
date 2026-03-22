7b9bbe011b398a601e67d550651c7191808fcbbb
Lorin Jacot <71598369+lorinjacot@users.noreply.github.com>
2026-03-22 20:09:17 +0100

    billiard: gjk nearest_tetrahedron calling nearest_triangle again

--- billiard/src/gjk.rs
+++ billiard/src/gjk.rs
@@ -81,12 +81,12 @@ fn nearest_triangle([c, b, a]: [Vec3; 3]) -> (Simplex, Vec3, bool) {
 
     let ab_normal = ab.cross(abc_normal);
     if ab_normal.dot(ao) > 0.0 {
-        return (Simplex::Line([b, a]), ab_normal, false);
+        return nearest_line([b, a]);
     }
 
     let ac_normal = abc_normal.cross(ac);
     if ac_normal.dot(ao) > 0.0 {
-        return (Simplex::Line([c, a]), ac_normal, false);
+        return nearest_line([c, a]);
     }
 
     if abc_normal.dot(ao) > 0.0 {
@@ -104,17 +104,17 @@ fn nearest_tetrahedron([d, c, b, a]: [Vec3; 4]) -> (Simplex, Vec3, bool) {
 
     let abc_normal = ab.cross(ac);
     if abc_normal.dot(ao) > 0.0 {
-        return (Simplex::Triangle([c, b, a]), abc_normal, false);
+        return nearest_triangle([c, b, a]);
     }
 
     let acd_normal = ac.cross(ad);
     if acd_normal.dot(ao) > 0.0 {
-        return (Simplex::Triangle([d, c, a]), acd_normal, false);
+        return nearest_triangle([d, c, a]);
     }
 
     let adb_normal = ad.cross(ab);
     if adb_normal.dot(ao) > 0.0 {
-        return (Simplex::Triangle([b, d, a]), adb_normal, false);
+        return nearest_triangle([b, d, a]);
     }
 
     (Simplex::Tetrahedron([d, c, b, a]), ao, true)

199f9c2a4d14d7f5fbe6d692bdd6e21d01ea6937
Lorin Jacot <71598369+lorinjacot@users.noreply.github.com>
2026-04-19 10:16:26 +0200

    sandbox: egui support

---
+++ sandbox/src/epa.rs
@@ -0,0 +1,219 @@
+#![allow(dead_code)]
+
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use glam::{Vec3, Vec4};
+
+use crate::{gjk::SupportPoint, shape::ConvexShape};
+
+const MAX_ITERATION: usize = 100;
+
+pub(crate) fn epa<S1: ConvexShape + ?Sized, S2: ConvexShape + ?Sized>(
+    shape1: &S1,
+    shape2: &S2,
+    tetrahedron: [SupportPoint; 4],
+) -> Vec4 {
+    let mut vertices = Vec::from(tetrahedron);
+    let mut faces = BinaryHeap::from(
+        [[3, 2, 1], [3, 1, 0], [3, 0, 2], [2, 0, 1]]
+            .map(|indices| Reverse(Face::from_vertex_indices(indices, &vertices))),
+    );
+
+    let mut unique_edges = Vec::new();
+    for _ in 0..MAX_ITERATION {
+        let closest_face = &faces.peek().unwrap().0;
+
+        let support = SupportPoint::new(shape1, shape2, closest_face.normal);
+        let distance_support = support.difference.dot(closest_face.normal);
+
+        if (distance_support - closest_face.distance).abs() < 1e-4 {
+            break;
+        }
+
+        // In order to keep the polyhedron convex, we need to remove all faces visible from `support`.
+        // This creates an hole whose border is made up of all edges appearing in only one of the removed faces.
+        faces.retain(|face| {
+            if face.0.normal.dot(support.difference) > 0.0 {
+                face.0.edges().into_iter().for_each(|(i, j)| {
+                    match unique_edges
+                        .iter()
+                        .enumerate()
+                        .find(|(_, edge)| **edge == (j, i))
+                    {
+                        Some((i, _)) => {
+                            unique_edges.swap_remove(i);
+                        }
+                        None => unique_edges.push((i, j)),
+                    }
+                });
+
+                false
+            } else {
+                true
+            }
+        });
+
+        let k = vertices.len();
+        vertices.push(support);
+        faces.extend(
+            unique_edges
+                .drain(..)
+                .map(|(i, j)| Reverse(Face::from_vertex_indices([i, j, k], &vertices))),
+        );
+    }
+
+    let closest_face = &faces.peek().unwrap().0;
+    closest_face.normal.extend(closest_face.distance)
+}
+
+#[derive(Debug)]
+struct Face {
+    indices: [usize; 3],
+    normal: Vec3,
+    distance: f32,
+}
+
+impl Face {
+    fn from_vertex_indices(vertex_indices: [usize; 3], vertices: &[SupportPoint]) -> Face {
+        let [a, b, c] = vertex_indices.map(|i| vertices[i].difference);
+        let mut normal = (b - a).cross(c - a).try_normalize().unwrap_or_else(|| {
+            dbg!(vertices);
+            todo!("abc is a line or a point")
+        });
+        let mut distance = a.dot(normal);
+        if distance < 0.0 {
+            normal = -normal;
+            distance = -distance;
+        }
+
+        Face {
+            indices: vertex_indices,
+            normal,
+            distance,
+        }
+    }
+
+    fn edges(&self) -> [(usize, usize); 3] {
+        [(0, 1), (1, 2), (2, 0)].map(|(i, j)| (self.indices[i], self.indices[j]))
+    }
+}
+
+impl PartialEq for Face {
+    fn eq(&self, other: &Self) -> bool {
+        self.distance == other.distance
+    }
+}
+
+impl PartialOrd for Face {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Eq for Face {}
+
+impl Ord for Face {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.distance.total_cmp(&other.distance)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use glam::vec3;
+
+    use crate::{
+        gjk::gjk_tetrahedron,
+        shape::{AxisAlignedBox, Ball},
+    };
+
+    use super::*;
+
+    #[test]
+    fn test_two_balls() {
+        // let origin = Ball {
+        //     center: Vec3::ZERO,
+        //     radius: 1.0,
+        // };
+
+        // assert!(gjk(&origin, &origin));
+
+        // let x = Ball {
+        //     center: Vec3::X,
+        //     radius: 1.0,
+        // };
+        // assert!(gjk(&x, &x));
+        // let tetrahedron = gjk_tetrahedron(&origin, &x).unwrap();
+        // dbg!(epa(&origin, &x, tetrahedron));
+        // assert!(gjk(&origin, &x));
+
+        // let three_x = Ball {
+        //     center: 3.0 * Vec3::X,
+        //     radius: 1.0,
+        // };
+        // assert!(!gjk(&origin, &three_x));
+        // assert!(gjk(&x, &three_x));
+
+        // let random_center = Ball {
+        //     center: vec3(-1.0312, 0.13312, 1.2),
+        //     radius: 1.0,
+        // };
+        // assert!(gjk(&origin, &random_center));
+        // assert!(!gjk(&x, &random_center));
+
+        // let radius = 2.343;
+        // let random_radius = Ball {
+        //     center: Vec3::Y * radius,
+        //     radius,
+        // };
+        // assert!(gjk(&origin, &random_radius));
+        // assert!(gjk(&x, &random_radius));
+        // assert!(!gjk(&three_x, &random_radius));
+    }
+
+    #[test]
+    fn test_ball_axis_aligned_boxes() {
+        let aab = AxisAlignedBox::from_center_dimension(Vec3::ZERO, 2.0, 2.0, 2.0);
+
+        let mut ball = Ball {
+            center: Vec3::ZERO,
+            radius: 1.0,
+        };
+
+        // let tetrahedron = gjk_tetrahedron(&aab, &ball).unwrap();
+        // dbg!(epa(&aab, &ball, tetrahedron));
+
+        ball.center = vec3(1.99, 0.0, 0.0);
+        let tetrahedron = gjk_tetrahedron(&aab, &ball).unwrap();
+        let separating_vector = epa(&aab, &ball, tetrahedron);
+        assert_seperating_vector(ball.center.normalize(), 0.01, separating_vector);
+
+        ball.center = vec3(2.0, 0.0, 0.0);
+        let tetrahedron = gjk_tetrahedron(&aab, &ball).unwrap();
+        let separating_vector = epa(&aab, &ball, tetrahedron);
+        assert_seperating_vector(ball.center.normalize(), 0.0, separating_vector);
+
+        ball.center = vec3(1.70, 1.70, 0.0);
+        let tetrahedron = gjk_tetrahedron(&aab, &ball).unwrap();
+        let separating_vector = epa(&aab, &ball, tetrahedron);
+        assert_seperating_vector(ball.center.normalize(), 0.01, separating_vector);
+
+        ball.center = vec3(1.57, 1.57, 1.57);
+        let tetrahedron = gjk_tetrahedron(&aab, &ball).unwrap();
+        let separating_vector = epa(&aab, &ball, tetrahedron);
+        assert_seperating_vector(ball.center.normalize(), 0.01, separating_vector);
+    }
+
+    fn assert_seperating_vector(expected_direction: Vec3, expected_length: f32, actual: Vec4) {
+        assert!(
+            actual.truncate().abs_diff_eq(expected_direction, 1e-2),
+            "Expected {expected_direction}, got {}",
+            actual.truncate()
+        );
+        assert!(
+            (actual.w - expected_length).abs() <= 1e-4,
+            "Expected {expected_length}, got {}",
+            actual.w
+        );
+    }
+}
--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,3 +1,392 @@
+use std::iter::once;
+use std::sync::Arc;
+
+use glam::{vec3, vec4};
+use tonner::Context;
+use tonner::entity_component::EntityManager;
+use tonner::environment::{Environment, EnvironmentBuilder};
+use tonner::geometry::GeometryBuilder;
+use tonner::geometry::skin::SkinManager;
+use tonner::mesh::material::MaterialBuilder;
+use tonner::mesh::{MeshBuilder, MeshInstance};
+use tonner::renderer::Renderer;
+use tonner::renderer::camera::Camera;
+use tonner::renderer::light::LightManager;
+use tonner::scene_graph::SceneGraph;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop, OwnedDisplayHandle};
+use winit::window::{Window, WindowId};
+
+struct Scene {
+    context: Context,
+    scene_graph: SceneGraph,
+    triangle: MeshInstance,
+    camera: Camera,
+    skin_manager: SkinManager,
+    light_manager: LightManager,
+    environment: Environment,
+    renderer: Renderer,
+}
+
+impl Scene {
+    fn render(&mut self, texture_view: &wgpu::TextureView, encoder: &mut wgpu::CommandEncoder) {
+        self.renderer
+            .render(
+                &self.camera,
+                &texture_view,
+                &mut self.scene_graph,
+                &mut self.skin_manager,
+                [&self.triangle],
+                &mut self.light_manager,
+                &self.environment,
+                &self.context,
+                encoder,
+            )
+            .unwrap();
+    }
+}
+
+struct State {
+    scene: Scene,
+    window: Arc<Window>,
+    size: winit::dpi::PhysicalSize<u32>,
+    instance: wgpu::Instance,
+    surface: wgpu::Surface<'static>,
+    surface_format: wgpu::TextureFormat,
+    egui_state: egui_winit::State,
+    egui_renderer: egui_wgpu::Renderer,
+}
+
+impl State {
+    async fn new(display: OwnedDisplayHandle, window: Arc<Window>) -> State {
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::new_with_display_handle(
+            Box::new(display),
+        ));
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions::default())
+            .await
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor::default())
+            .await
+            .unwrap();
+
+        let size = window.inner_size();
+
+        let surface = instance.create_surface(window.clone()).unwrap();
+        let cap = surface.get_capabilities(&adapter);
+        let surface_format = cap.formats[0];
+
+        let egui_ctx = egui::Context::default();
+        let egui_state = egui_winit::State::new(
+            egui_ctx,
+            egui::ViewportId::ROOT,
+            &window,
+            Some(window.scale_factor() as f32),
+            window.theme(),
+            Some(device.limits().max_texture_dimension_2d as usize),
+        );
+
+        let egui_renderer = egui_wgpu::Renderer::new(
+            &device,
+            surface_format.remove_srgb_suffix(),
+            egui_wgpu::RendererOptions::default(),
+        );
+
+        let context = Context::from_device(device, queue);
+        let mut entity_manager = EntityManager::new();
+
+        let mut encoder =
+            context
+                .device()
+                .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                    label: Some("App::resumed command encoder"),
+                });
+
+        let mut scene_graph = SceneGraph::new(&context);
+        let renderer = Renderer::new(
+            size.width,
+            size.height,
+            wgpu::TextureFormat::Rgba8UnormSrgb,
+            &context,
+        );
+
+        let triangle = GeometryBuilder::new(3, 0)
+            .name("Triangle")
+            .positions([
+                vec3(0.0, -0.5, -5.0),
+                vec3(0.5, 0.5, -5.0),
+                vec3(-0.5, 0.5, -5.0),
+            ])
+            .unwrap()
+            .build(&context)
+            .unwrap();
+
+        let red = MaterialBuilder::default()
+            .name("red")
+            .base_color_factor(vec4(1.0, 0.0, 0.0, 1.0))
+            .build(&context);
+
+        let red_triangle = MeshBuilder::default()
+            .name("Triangle")
+            .primitive(triangle, red)
+            .build(&context)
+            .unwrap();
+
+        let triangle = entity_manager.new_entity();
+        scene_graph.add(triangle, None);
+        let triangle = red_triangle.new_instance(triangle);
+
+        let camera = entity_manager.new_entity();
+        scene_graph.add(camera, None);
+        let camera = Camera::new(camera);
+
+        let skin_manager = SkinManager::new(&context);
+        let light_manager = LightManager::new(&context);
+        let environment = EnvironmentBuilder::default().build(&context, &mut encoder);
+
+        context.queue().submit([encoder.finish()]);
+
+        let scene = Scene {
+            context,
+            scene_graph,
+            triangle,
+            camera,
+            skin_manager,
+            light_manager,
+            environment,
+            renderer,
+        };
+
+        let state = State {
+            scene,
+            window,
+            size,
+            instance,
+            surface,
+            surface_format,
+            egui_state,
+            egui_renderer,
+        };
+
+        state.configure_surface();
+
+        state
+    }
+
+    fn configure_surface(&self) {
+        let surface_config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: self.surface_format,
+            view_formats: vec![
+                self.surface_format.remove_srgb_suffix(),
+                self.surface_format.add_srgb_suffix(),
+            ],
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            width: self.size.width,
+            height: self.size.height,
+            desired_maximum_frame_latency: 2,
+            present_mode: wgpu::PresentMode::AutoVsync,
+        };
+        self.surface
+            .configure(self.scene.context.device(), &surface_config);
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.size = new_size;
+        self.configure_surface();
+    }
+
+    fn on_window_event(&mut self, event: &winit::event::WindowEvent) -> bool {
+        let response = self.egui_state.on_window_event(&self.window, event);
+        if response.repaint {
+            self.window.request_redraw();
+        }
+        response.consumed
+    }
+
+    fn on_mouse_motion(&mut self, delta: (f64, f64)) -> bool {
+        self.egui_state.on_mouse_motion(delta)
+    }
+
+    fn render(&mut self) {
+        let raw_input = self.egui_state.take_egui_input(&self.window);
+
+        let full_output = self.egui_state.egui_ctx().run_ui(raw_input, |_ui| {
+            // egui::Panel::right("right panel").show_inside(ui, |ui| {
+            //     ui.label("Hello world!");
+            //     if ui.button("Click me").clicked() {
+            //         println!("Clicked");
+            //     }
+            // });
+        });
+
+        self.egui_state
+            .handle_platform_output(&self.window, full_output.platform_output);
+
+        let surface_texture = match self.surface.get_current_texture() {
+            wgpu::CurrentSurfaceTexture::Success(texture) => texture,
+            wgpu::CurrentSurfaceTexture::Occluded | wgpu::CurrentSurfaceTexture::Timeout => return,
+            wgpu::CurrentSurfaceTexture::Suboptimal(_) | wgpu::CurrentSurfaceTexture::Outdated => {
+                self.configure_surface();
+                return;
+            }
+            wgpu::CurrentSurfaceTexture::Validation => {
+                unreachable!("No error scope registered, so validation errors will panic")
+            }
+            wgpu::CurrentSurfaceTexture::Lost => {
+                self.surface = self.instance.create_surface(self.window.clone()).unwrap();
+                self.configure_surface();
+                return;
+            }
+        };
+        let srgb_texture_view = surface_texture
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor {
+                format: Some(self.surface_format.add_srgb_suffix()),
+                ..Default::default()
+            });
+
+        let mut encoder = self
+            .scene
+            .context
+            .device()
+            .create_command_encoder(&Default::default());
+
+        self.scene.render(&srgb_texture_view, &mut encoder);
+
+        let clipped_primitives = self
+            .egui_state
+            .egui_ctx()
+            .tessellate(full_output.shapes, full_output.pixels_per_point);
+
+        let screen_descriptor = egui_wgpu::ScreenDescriptor {
+            size_in_pixels: [self.size.width, self.size.height],
+            pixels_per_point: full_output.pixels_per_point,
+        };
+        let command_buffers = self.egui_renderer.update_buffers(
+            self.scene.context.device(),
+            self.scene.context.queue(),
+            &mut encoder,
+            &clipped_primitives,
+            &screen_descriptor,
+        );
+        full_output.textures_delta.free.iter().for_each(|id| {
+            self.egui_renderer.free_texture(&id);
+        });
+        full_output
+            .textures_delta
+            .set
+            .into_iter()
+            .for_each(|(id, delta)| {
+                self.egui_renderer.update_texture(
+                    self.scene.context.device(),
+                    self.scene.context.queue(),
+                    id,
+                    &delta,
+                );
+            });
+
+        let rgb_texture_view = surface_texture
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor {
+                format: Some(self.surface_format.remove_srgb_suffix()),
+                ..Default::default()
+            });
+        let mut render_pass = encoder
+            .begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("State::render() render pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &rgb_texture_view,
+                    depth_slice: None,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Load,
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+                multiview_mask: None,
+            })
+            .forget_lifetime();
+        self.egui_renderer
+            .render(&mut render_pass, &clipped_primitives, &screen_descriptor);
+        drop(render_pass);
+
+        self.scene
+            .context
+            .queue()
+            .submit(command_buffers.into_iter().chain(once(encoder.finish())));
+        self.window.pre_present_notify();
+        surface_texture.present();
+    }
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+
+        let state = pollster::block_on(State::new(
+            event_loop.owned_display_handle(),
+            window.clone(),
+        ));
+        self.state = Some(state);
+
+        window.request_redraw();
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        let state = self.state.as_mut().unwrap();
+        let consumed = state.on_window_event(&event);
+        if !consumed {
+            match event {
+                WindowEvent::CloseRequested => {
+                    event_loop.exit();
+                }
+                WindowEvent::RedrawRequested => {
+                    state.render();
+                    state.window.request_redraw();
+                }
+                WindowEvent::Resized(size) => {
+                    state.resize(size);
+                }
+                _ => (),
+            }
+        }
+    }
+    fn device_event(
+        &mut self,
+        _event_loop: &ActiveEventLoop,
+        _device_id: winit::event::DeviceId,
+        event: winit::event::DeviceEvent,
+    ) {
+        match event {
+            winit::event::DeviceEvent::MouseMotion { delta } => {
+                let state = self.state.as_mut().unwrap();
+                state.on_mouse_motion(delta);
+            }
+            _ => (),
+        }
+    }
+}
+
+#[derive(Default)]
+struct App {
+    state: Option<State>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
 }

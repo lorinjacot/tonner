8d64791e277366d744548073a4700277c2e3efa8
Lorin Jacot <71598369+lorinjacot@users.noreply.github.com>
2026-05-28 21:34:27 +0200

    entropie: forces and better Constraints

--- entropie/src/lib.rs
+++ entropie/src/lib.rs
@@ -3,8 +3,11 @@ pub use object::Particle;
 pub use transform::Transform;
 
 mod aabb;
+pub mod body;
 pub mod collision;
 pub mod constraint;
+pub mod force;
 mod object;
 pub mod shape;
 mod transform;
+
